a0912de47cb8884005e0cec9cf16fd032100e56b
LCBecker <PersephoneDown@gmail.com>
2020-04-12 11:35:58 -0400

    Minor style changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import { AppBar, CssBaseline, Toolbar, Typography, makeStyles } from '@material-ui/core';
+
+import LineGraph from './components/LineGraph';
+import ParameterSidebar from './components/ParameterSidebar';
 import React from 'react';
 import { connect } from 'react-redux';
-import ParameterSidebar from './components/ParameterSidebar';
-import { makeStyles, CssBaseline, Typography, AppBar, Toolbar } from '@material-ui/core';
-import LineGraph from './components/LineGraph';
 
 const drawerWidth = 300;
 
@@ -32,7 +33,7 @@ function App(props) {
       <AppBar position="fixed" className={classes.appBar}>
         <Toolbar>
           <Typography variant="h6" noWrap>
-          Penn Medicine COVID-19 Hospital Impact Model for Epidemics (CHIME)
+            Penn Medicine COVID-19 Hospital Impact Model for Epidemics (CHIME)
           </Typography>
         </Toolbar>
       </AppBar>
@@ -40,16 +41,16 @@ function App(props) {
       <main className={classes.content}>
         <div className={classes.toolbar} />
         <Typography paragraph>
-        This tool was developed by the Predictive Healthcare team at Penn Medicine 
-        to assist hospitals and public health officials with hospital capacity planning.
+          This tool was developed by the Predictive Healthcare team at Penn Medicine
+          to assist hospitals and public health officials with hospital capacity planning.
         </Typography>
         <Typography paragraph>
-        Notice: There is a high degree of uncertainty about the details of COVID-19 infection, 
-        transmission, and the effectiveness of social distancing measures. Long-term projections 
-        made using this simplified model of outbreak progression should be treated with extreme caution.
+          Notice: There is a high degree of uncertainty about the details of COVID-19 infection,
+          transmission, and the effectiveness of social distancing measures. Long-term projections
+          made using this simplified model of outbreak progression should be treated with extreme caution.
         </Typography>
         <Typography paragraph>
-          The estimated number of currently infected individuals is {props.infected}. This is based on current inputs for 
+          The estimated number of currently infected individuals is {props.infected}. This is based on current inputs for
   Hospitalizations ({props.currentPatients}), Hospitalization rate ({props.doublingTime}%), Region size ({props.regionalPopulation}), and Hospital market share ({props.hospitalMarket}%).
         </Typography>
         <LineGraph />
--- src/components/ParameterSidebar.js
+++ src/components/ParameterSidebar.js
@@ -1,13 +1,14 @@
+import { resetParams, setCurrentPatients, setDoublingTime, setHospitalMarket, setRegionalPopulation, setSocialDistancing } from '../redux/actions';
+
+import Button from '@material-ui/core/Button';
+import Divider from '@material-ui/core/Divider';
+import Drawer from '@material-ui/core/Drawer';
+import InfoIcon from '@material-ui/icons/Info';
+import ParameterInput from './ParameterInput';
 import React from 'react';
+import Typography from '@material-ui/core/Typography'
 import { connect } from 'react-redux';
 import { makeStyles } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
-import Divider from '@material-ui/core/Divider';
-import Typography from '@material-ui/core/Typography'
-import Button from '@material-ui/core/Button';
-import ParameterInput from './ParameterInput';
-import InfoIcon from '@material-ui/icons/Info';
-import { setRegionalPopulation, setHospitalMarket, setCurrentPatients, setDoublingTime, setSocialDistancing, resetParams } from '../redux/actions';
 
 const drawerWidth = 300;
 
@@ -21,6 +22,9 @@ const useStyles = makeStyles((theme) => ({
     padding: '0 10px',
   },
   toolbar: theme.mixins.toolbar,
+  sidebarHeader: {
+    marginTop: '20px',
+  }
 }));
 
 const ParameterSidebar = (props) => {
@@ -34,37 +38,37 @@ const ParameterSidebar = (props) => {
       classes={{ paper: classes.drawerPaper }}
     >
       <div className={classes.toolbar}>
-        <Typography>CHIME <a href="https://github.com/CodeForPhilly/chime/releases/tag/v1.1.1">v1.1.2</a> (2020-04-01)</Typography>
+        <Typography align={"center"} className={classes.sidebarHeader}>CHIME <a href="https://github.com/CodeForPhilly/chime/releases/tag/v1.1.1">v1.1.2</a> (2020-04-01)</Typography>
       </div>
       <Divider />
-        <Typography variant="subtitle1">
-          Hospital Parameters
+      <Typography variant="subtitle1">
+        Hospital Parameters
           <a href="https://code-for-philly.gitbook.io/chime/what-is-chime/parameters#hospital-parameters" target="_blank" rel="noopener noreferrer">
-            <InfoIcon fontSize="small"/>
-          </a>
-          </Typography>
-        <ParameterInput 
-          id="regional-population" 
-          label="Regional Population"
-          value={props.regionalPopulation}
-          minValue={1}
-          onInputChange={ props.setRegionalPopulation } 
-        />
-        <ParameterInput 
-          id="hospital-market" 
-          label="Hospital Market Share (%)"
-          value={props.hospitalMarket}
-          minValue={0.001}
-          maxValue={100.0}
-          onInputChange={ props.setHospitalMarket } 
-        />
-        <ParameterInput 
-          id="current-covid-patients" 
-          label="Currently Hospitalized COVID-19 Patients"
-          value={props.currentPatients}
-          minValue={0}
-          onInputChange={ props.setCurrentPatients } 
-        />
+          <InfoIcon fontSize="small" />
+        </a>
+      </Typography>
+      <ParameterInput
+        id="regional-population"
+        label="Regional Population"
+        value={props.regionalPopulation}
+        minValue={1}
+        onInputChange={props.setRegionalPopulation}
+      />
+      <ParameterInput
+        id="hospital-market"
+        label="Hospital Market Share (%)"
+        value={props.hospitalMarket}
+        minValue={0.001}
+        maxValue={100.0}
+        onInputChange={props.setHospitalMarket}
+      />
+      <ParameterInput
+        id="current-covid-patients"
+        label="Currently Hospitalized COVID-19 Patients"
+        value={props.currentPatients}
+        minValue={0}
+        onInputChange={props.setCurrentPatients}
+      />
       <Divider />
       <Typography variant="subtitle1">
         Spread and Contact Parameters
@@ -72,23 +76,23 @@ const ParameterSidebar = (props) => {
           <InfoIcon fontSize="small" />
         </a>
       </Typography>
-      <ParameterInput 
-        id="doubling-time" 
+      <ParameterInput
+        id="doubling-time"
         label="Doubling time in days (up to today)"
         value={props.doublingTime}
         minValue={0}
-        onInputChange={ props.setDoublingTime } 
+        onInputChange={props.setDoublingTime}
       />
-      <ParameterInput 
-        id="social-distancing" 
+      <ParameterInput
+        id="social-distancing"
         label="Social distancing (% reduction in social contact going forward"
         value={props.socialDistancing}
         minValue={0}
         maxValue={100}
-        onInputChange={ props.setSocialDistancing } 
+        onInputChange={props.setSocialDistancing}
       />
-      <Button onClick={() => props.resetParams() }>Reset</Button>
-    </Drawer> 
+      <Button onClick={() => props.resetParams()}>Reset</Button>
+    </Drawer>
   )
 }
 
